ab7faf613092a0c75779e85c101cb86a3ff2e8be
lura00 <ludvig.skinstad@gmail.com>
2023-05-15 20:29:03 +0200

    added pipeline to repo

--- vpc.tf
+++ vpc.tf
@@ -3,9 +3,9 @@
 
 
 provider "google" {
-  project = var.project_id
-  region  = var.region
-  credentials = file("breakingbad-gke-gradjob-30d6e875263f.json")
+  project     = var.project_id
+  region      = var.region
+  credentials = ${{ secrets.GKE_KEY }}
 }
 
 # VPC
@@ -20,4 +20,35 @@ resource "google_compute_subnetwork" "subnet" {
   region        = var.region
   network       = google_compute_network.vpc.name
   ip_cidr_range = "10.10.0.0/24"
+}
+
+resource "google_compute_route" "egress_internet" {
+  name             = "egress-internet"
+  dest_range       = "0.0.0.0/0"
+  network          = google_compute_network.vpc.name
+  next_hop_gateway = "default-internet-gateway"
+}
+
+resource "google_compute_router" "router" {
+  name    = "${var.project_id}-router"
+  region  = google_compute_subnetwork.subnet.region
+  network = google_compute_network.vpc.name
+}
+
+resource "google_compute_router_nat" "nat_router" {
+  name                               = "${google_compute_subnetwork.subnet.name}-nat-router"
+  router                             = google_compute_router.router.name
+  region                             = google_compute_router.router.region
+  nat_ip_allocate_option             = "AUTO_ONLY"
+  source_subnetwork_ip_ranges_to_nat = "LIST_OF_SUBNETWORKS"
+
+  subnetwork {
+    name                    = google_compute_subnetwork.subnet.name
+    source_ip_ranges_to_nat = ["ALL_IP_RANGES"]
+  }
+
+  log_config {
+    enable = true
+    filter = "ERRORS_ONLY"
+  }
 }
