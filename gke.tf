24d50e11c2f226d31d3e5e612fd1e9c71fb66c38
lura00 <ludvig.skinstad@gmail.com>
2023-05-26 11:38:14 +0200

    changed some in infrastructure test 2

--- gke.tf
+++ gke.tf
@@ -9,7 +9,7 @@ resource "google_container_cluster" "primary" {
   # separately managed node pools. So we create the smallest possible default
   # node pool and immediately delete it.
   remove_default_node_pool = true
-  initial_node_count       = 4
+  initial_node_count       = 2
 
   network    = google_compute_network.vpc.name
   subnetwork = google_compute_subnetwork.subnet.name
